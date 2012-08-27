6a507a9f072e980dd13b811fac0386e26bddc1f4
smith24 <smith24@mail.ru>
2012-08-27 22:36:16 +0400

    123
    123123

--- client/javascript/componentsOfUsersMVC/individualSheduleMVC.js
+++ client/javascript/componentsOfUsersMVC/individualSheduleMVC.js
@@ -7,31 +7,18 @@ function ControllerOfIndividualShedule () {
 	//5. bind MAKE_ORDER
 	//6. ? bind CLOSE ?
 	//7. 
-	this.modelOfIndividualShedule = new ModelOfIndividualShedule();
+	this.modelOfIndividualShedule = new ModelOfIndividualShedule(); 
 	this.viewOfIndividualShedule = new ViewOfIndividualShedule();
-}
-
-
-function ModelOfIndividualShedule () {
-	//0. constructor = getIndividualShedule() + getEvent()
-	//1. getIndividualShedule = function of AJAX (aud, week1, week2). return HTML of individual shedule of selected aud.
-	//3. showOrder = function (...pre-set of parametrs to fill order's field ...). fill field of orderMenuWindow
-	//4. checkOrderParametrs = function (). validate paametrs of order. (? AJAX to check on server complicated parametrs, or only client checker ?)
-	//5. makeOrder = function of AJAX (...full set of order's parametrs...). send to server the order pack . return status: 
-					// if OK -> order added in stack -> also return HTML from server and call modelOfUserRequests.addNewRequest()
-					// else say some attention message "Your request is suck. try again.."
-	//6.
-}
-
-function ViewOfIndividualShedule () {
 	this.showMe = function (idAuditory, dataStart,guest) { // great fucking function. need to be fixed!
 		function showTableWithOtherTime(idAuditory, dataStart,guest) {
 			var dayTrans = ""; // день недели для обращения к нужной таблице бд латинскими буквами
+		//function showTableWithOtherTime(idAuditory, dataStart,guest) {
+			var dayTrans = ""; // day of week for reqiest to table of database in latin12 i upload this again
 			var tmp = cur_head; 
 			if (dataStart) {
 				var month;
 				var day;
-				var chet = ""; // указывает четная или нечетная неделя even or odd
+				var chet = ""; // указывает четная или нечетная неделя even or odd111
 				var year;
 				var d = new Date((86400 * 7 + dataStart) * 1000);
 				month = d.getMonth() + 1;
@@ -169,6 +156,21 @@ function ViewOfIndividualShedule () {
 			
 		}
 	}
+}
+
+
+function ModelOfIndividualShedule () {
+	//0. constructor = getIndividualShedule() + getEvent()
+	//1. getIndividualShedule = function of AJAX (aud, week1, week2). return HTML of individual shedule of selected aud.
+	//3. showOrder = function (...pre-set of parametrs to fill order's field ...). fill field of orderMenuWindow
+	//4. checkOrderParametrs = function (). validate paametrs of order. (? AJAX to check on server complicated parametrs, or only client checker ?)
+	//5. makeOrder = function of AJAX (...full set of order's parametrs...). send to server the order pack . return status: 
+					// if OK -> order added in stack -> also return HTML from server and call modelOfUserRequests.addNewRequest()
+					// else say some attention message "Your request is suck. try again.."
+	//6.
+}
+
+function ViewOfIndividualShedule () {
 	this.hideMe = function () {
 		
 	}
